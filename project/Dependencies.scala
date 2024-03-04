4a01d16edbd4517e1e7bb5fd98783b06e24360c3
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2024-03-04 17:19:08 +0000

    Bump dependency

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ object Dependencies {
   lazy val pureConfigVersion = "0.17.4"
   lazy val logbackVersion = "2.20.0"
 
-  lazy val awsDynamoDbClient = "uk.gov.nationalarchives" %% "da-dynamodb-client" % "0.1.23"
+  lazy val awsDynamoDbClient = "uk.gov.nationalarchives" %% "da-dynamodb-client" % "0.1.37"
   lazy val dynamoFormatters = "uk.gov.nationalarchives" %% "dynamo-formatters" % "0.0.1"
   lazy val log4jSlf4j = "org.apache.logging.log4j" % "log4j-slf4j-impl" % logbackVersion
   lazy val log4jCore = "org.apache.logging.log4j" % "log4j-core" % logbackVersion
